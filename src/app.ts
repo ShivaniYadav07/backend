8f68f618e9e429f769b6822b92ae28424ba5b675
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2025-11-17 20:16:46 +0530

    fix: update DATABASE_URL and CORS origin for production environment

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 app.use(cors({
-  origin: "*", // frontend URL
+  origin: "http://localhost:3000", // frontend URL
   credentials: true
 }));
 app.use(cookieParser());
